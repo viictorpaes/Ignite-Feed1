99c0c007aa285f67b798e6041cc6fb9ff9ca5e09
viictorpaes <victorjosepaes@gmail.com>
2025-05-10 11:36:24 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,79 @@
+import { Header } from "./components/Header";
+import { Post } from "./components/Post";
+import { Sidebar } from "./components/Sidebar";
+
+import styles from './app.module.css';
+
+import './global.css';
+
+
+
+const posts = [
+  {
+    id: 1,
+    author:{
+      avatarUrl: 'https://github.com/viictorpaes.png',
+      name: 'Victor Paes',
+      role: 'CEO @Humores',
+    },
+    content:[
+      {type: 'paragraph', content: 'Opa jovana bom dia  👋'},
+      {type: 'paragraph', content: 'Ta ai a mais nova'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},     
+      {type: 'link', content: 'github.com/viictorpaes'},
+    ],
+    publishedAt: new Date('2025-03-02 20:00:00'),
+  },
+  {
+    id: 2,
+    author:{
+      avatarUrl: 'https://github.com/MirellaMonteiroM.png',
+      name: 'Mirella Borba',
+      role: 'CEO @Viadagem',
+    },
+    content:[
+      {type: 'paragraph', content: 'Divou'},
+      {type: 'paragraph', content: 'Giu eh muito diva'},     
+      {type: 'link', content: 'instagram.com/paesvp'},
+    ],
+    publishedAt: new Date('2025-03-4 20:00:00'),
+  },
+  {
+    id: 3,
+    author:{
+      avatarUrl: 'https://github.com/viiniciuspaes.png',
+      name: 'Vinicius Paes',
+      role: 'CEO @Sky jungle',
+    },
+    content:[
+      {type: 'paragraph', content: 'Fala👋'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},     
+      {type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2025-03-4 20:00:00'),
+  },
+];
+
+export function App() {
+  return (
+  <div>
+    <Header/>
+
+      <div className={styles.wrapper}>
+        <Sidebar/>
+        <main>
+           {posts.map(post =>{
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+           })}
+        </main>
+      </div>
+    </div>
+    
+  )
+}
---
+++ src/components/Comment.jsx
@@ -0,0 +1,35 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+import { Avatar } from './Avatar';
+
+export function Comment(){
+    return(
+        <div className={styles.comment}>
+            <Avatar hasBorder={false} src="https://github.com/viictorpaes.png" alt="" />
+            
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Victor Paes</strong>
+                            <time title="11 de fevereiro às 12:00h" dateTime="2025-02-11 12:00:00">Cerca de 1hr atrás</time>
+                        </div>
+
+                        <button title="Deletar Comentário">
+                        <Trash size={24}/>
+                        </button>
+                    </header>
+
+                    <p>Divou mano divou 👏👏</p>
+                </div>
+            
+                <footer>
+                    <button>
+                        <ThumbsUp/>
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
